989f41db978f0fa151828e03cdb4e33d0b33f0c5
youliang <tan_you_liang@hotmail.com>
2023-02-26 00:33:34 -0500

    Create a pybullet tennis court env
    Signed-off-by: youliang <tan_you_liang@hotmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # tennisbot-rl
+
+The original tennisbot model is obtained from https://github.com/CORE-Robotics-Lab/Wheelchair-Tennis-Robot
+
+## Installation
+
+install dependencies
+ - pybullet
+ - gymnasium (new version of OpenAI gym)
+ - simple_pid
+
+Install the py pkg
+```bash
+cd tennisbot
+pip install -e . 
+```
+
+## Run Demo
+
+```bash
+cd ..
+```
+
+Run pybullet playground
+```bash
+python3 playground.py
+```
+
+Run gym training
+```bash
+python3 main.py
+```
---
+++ tennisbot/resources/objects.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import pybullet as p
+import os
+
+##############################################################################
+class Plane:
+    def __init__(self, client, pos=[0, 0, 0]):
+        f_name = os.path.join(os.path.dirname(__file__), 'simpleplane.urdf')
+        p.loadURDF(
+            fileName=f_name, basePosition=pos, physicsClientId=client)
+
+##############################################################################
+
+class Court:
+    def __init__(self, client, pos=[0, 0, 0]):
+        f_name = os.path.join(os.path.dirname(__file__), 'court.urdf')
+        self.id = p.loadURDF(
+            fileName=f_name, basePosition=pos, physicsClientId=client)
+
+        # Make bouncey court
+        p.changeDynamics(self.id, -1, restitution=0.9)
+        p.changeDynamics(self.id, -1, lateralFriction=0.2)
+        p.changeDynamics(self.id, -1, rollingFriction=0.001)
+        
+        # add court texture
+        f_name = os.path.join(os.path.dirname(__file__), 'court_mod.png')
+        texture = p.loadTexture(f_name)
+        p.changeVisualShape(self.id, -1, textureUniqueId=texture)
+
+##############################################################################
+class Ball:
+    def __init__(self, client, pos=[0, 0, 0]):
+        f_name = os.path.join(os.path.dirname(__file__), 'ball.urdf')
+        self.id = p.loadURDF(  
+            fileName=f_name, basePosition=pos, physicsClientId=client)
+
+        # Make bouncey ball
+        p.changeDynamics(self.id, -1, restitution=0.9)
+        p.changeDynamics(self.id, -1, lateralFriction=0.2)
+        p.changeDynamics(self.id, -1, rollingFriction=0.001)
