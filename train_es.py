ab1c7fd5c579fc2de953bbbcc2308b1b3e2845d0
youliang <tan_you_liang@hotmail.com>
2023-04-28 11:32:51 -0400

    reorganized repo
    Signed-off-by: youliang <tan_you_liang@hotmail.com>

--- train_es.py
+++ train_es.py
@@ -1,3 +1,7 @@
+"""
+Source repo: https://github.com/enajx/ES
+"""
+
 import time
 import argparse
 import sys
@@ -10,7 +14,7 @@ import pybullet_envs
 import tennisbot
 
 from tennisbot.ES.evolution_strategy_static import EvolutionStrategyStatic
-from tennisbot.ES.policies import MLP, CNN,GatedCNN
+from tennisbot.ES.policies import MLP, CNN, GatedCNN
 import tennisbot.ES.fitness_functions
 
 
@@ -22,26 +26,28 @@ def main(argv):
     parser = argparse.ArgumentParser()
     # env = gym.make('Tennisbot-v0')
 
-    
-    parser.add_argument('--environment', type=str, default='Tennisbot-v0', metavar='', help='Environment: any OpenAI Gym or pyBullet environment may be used')
-    parser.add_argument('--popsize', type=int,  default = 200, metavar='', help='Population size.') 
-    parser.add_argument('--print_every', type=int, default = 1, metavar='', help='Print and save every N steps.') 
-    parser.add_argument('--lr', type=float,  default = 0.2, metavar='', help='ES learning rate.') 
-    parser.add_argument('--decay', type=float,  default = 0.995, metavar='', help='ES decay.')  
-    parser.add_argument('--sigma', type=float,  default = 0.1, metavar='', help='ES sigma: modulates the amount of noise used to populate each new generation') 
-    parser.add_argument('--generations', type=int, default= 300, metavar='', help='Number of generations that the ES will run.')
-    parser.add_argument('--folder', type=str, default='weights', metavar='', help='folder to store the evolved weights ')
-    parser.add_argument('--threads', type=int, metavar='', default = -1, help='Number of threads used to run evolution in parallel.')
+    parser.add_argument('--environment', type=str, default='Tennisbot-v0', metavar='',
+                        help='Environment: any OpenAI Gym or pyBullet environment may be used')
+    parser.add_argument('--popsize', type=int,  default=200, metavar='', help='Population size.')
+    parser.add_argument('--print_every', type=int, default=1, metavar='', help='Print and save every N steps.')
+    parser.add_argument('--lr', type=float,  default=0.2, metavar='', help='ES learning rate.')
+    parser.add_argument('--decay', type=float,  default=0.995, metavar='', help='ES decay.')
+    parser.add_argument('--sigma', type=float,  default=0.1, metavar='',
+                        help='ES sigma: modulates the amount of noise used to populate each new generation')
+    parser.add_argument('--generations', type=int, default=300, metavar='',
+                        help='Number of generations that the ES will run.')
+    parser.add_argument('--folder', type=str, default='weights', metavar='',
+                        help='folder to store the evolved weights ')
+    parser.add_argument('--threads', type=int, metavar='', default=-1,
+                        help='Number of threads used to run evolution in parallel.')
 
-    
     args = parser.parse_args()
-
 
     if not exists(args.folder):
         mkdir(args.folder)
 
     # Look up observation and action space dimension
-    env = gym.make(args.environment)    
+    env = gym.make(args.environment)
     # if len(env.observation_space.shape) == 3:     # Pixel-based environment
     #     pixel_env = True
     # elif len(env.observation_space.shape) == 1:   # State-based environment
@@ -77,22 +83,21 @@ def main(argv):
     # else:
 
     p = GatedCNN(input_dim, action_dim)
-    print("our action diom," ,action_dim)
+    print("our action diom,", action_dim)
     print("our input dim", input_dim)
-
 
     # Initialise the EvolutionStrategy class
     print('\nInitilisating static-network ES for ' + str(args.environment))
     es = EvolutionStrategyStatic(
-            p.get_weights(),
-            args.environment,
-            input_size=input_dim,
-            population_size=args.popsize,
-            sigma=args.sigma,
-            learning_rate=args.lr,
-            decay=args.decay,
-            num_threads=args.threads
-        )
+        p.get_weights(),
+        args.environment,
+        input_size=input_dim,
+        population_size=args.popsize,
+        sigma=args.sigma,
+        learning_rate=args.lr,
+        decay=args.decay,
+        num_threads=args.threads
+    )
 
     # Start the evolution
     tic = time.time()
@@ -101,7 +106,7 @@ def main(argv):
     toc = time.time()
     print('\nEvolution took: ', int(toc-tic), ' seconds\n')
 
-
+##############################################################################
 
 if __name__ == '__main__':
     main(sys.argv)
